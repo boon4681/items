24b1da22d7c8765de528dd3e30108eb7735d6550
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-20 01:35:40 +0700

    item

---
+++ src/item.py
@@ -0,0 +1,18 @@
+from src.color import mix, to_rgb
+from src.resource import Resource
+import cv2
+
+def spawn_egg(res:Resource,colors,name):
+    model = res.readModel('item/template_spawn_egg')
+
+    im = res.loadTexture(model['textures']['layer0'],rgba=True)
+    im2 = res.loadTexture(model['textures']['layer1'],rgba=True)
+
+    im = mix(im,to_rgb(colors[name]['primary_color']))
+    im2 = mix(im2,to_rgb(colors[name]['secondary_color']))
+
+    for i in range(3):
+        im[:,:,i] = im[:,:,i]*(im[:,:,3]/255)
+        im2[:,:,i] = im2[:,:,i]*(im2[:,:,3]/255)
+    im = im + im2
+    return cv2.cvtColor(im,cv2.COLOR_RGBA2BGRA)
